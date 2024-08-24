1a085b1b0bb8022d62f7703ab8eb65d1c95be0a6
Laszlu <maurice.ferreyra@gmail.com>
2024-08-24 15:36:38 +0200

    v1

--- FileCrawler.py
+++ FileCrawler.py
@@ -28,8 +28,18 @@ def print_intro(backup_date_str):
     print('###############################################################################')
 
 
+def print_outro(count):
+    print('###############################################################################')
+    print('FILE CHECKER DONE')
+    print('FILES TO BACKUP: ', count)
+    print('###############################################################################')
+
+
 def check_files(backup_date_str, directory_path):
     backup_time = datetime.strptime(backup_date_str, '%Y-%m-%d %H:%M:%S')
+
+    count = 0
+    open(backup_log_path, 'w').close()
 
     for root, dir_names, file_names in os.walk(directory_path):
         for file_name in file_names:
@@ -40,13 +50,14 @@ def check_files(backup_date_str, directory_path):
                     continue
                 modify_time = datetime.fromtimestamp(os.path.getmtime(os.path.join(root, file_name)))
                 if modify_time > backup_time:
-                    print('File: ', os.path.join(root, file_name))
-                    print(modify_time)
-                    print(backup_time)
-                    print('File was modified after last backup')
+                    # print('File: ', os.path.join(root, file_name))
+                    # print(modify_time)
+                    # print(backup_time)
+                    # print('File was modified after last backup')
                     with open(backup_log_path, 'a') as f:
                         f.write(os.path.join(root, file_name))
                         f.write('\n')
+                    count += 1
             except FileNotFoundError:
                 print(os.path.join(root, file_name), 'not found!')
             finally:
@@ -59,16 +70,18 @@ def check_files(backup_date_str, directory_path):
             if (dir_name == 'Documents' or dir_name == 'Desktop' or dir_name == 'Downloads' or
                     dir_name == 'Pictures' or dir_name == 'Postman' or
                     dir_name == 'Projects' or dir_name == 'Public'):
-                print('\n------------------------------------------------------------')
-                # os.path.join(root, dir_name)
-                print('directory: ', dir_name, '\n')
-                print('Path: ', dir_path, '\n')
+                # print('\n------------------------------------------------------------')
+                # # os.path.join(root, dir_name)
+                # print('directory: ', dir_name, '\n')
+                # print('Path: ', dir_path, '\n')
                 check_files(backup_date_str, os.path.join(root, dir_name))
+    return count
 
 
 if __name__ == '__main__':
     backup_date = get_backup_date()
     print_intro(backup_date)
-    check_files(backup_date, base_path)
+    file_count = check_files(backup_date, base_path)
     write_backup_date()
+    print_outro(file_count)
 
